b343fd2648d9586f4fbafbee1e9606a1c76481fd
Leon <leon.lama2002@gmail.com>
2024-09-25 15:04:32 +0545

    mobile navigation implementation complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleMenuPress = () => {
+    setOpenMenu((prev) => !prev);
+  };
+
+  const handleMobileNavigationsClick = () => {
+    setOpenMenu(false);
+  };
+
+  const stopPropagation = (event) => {
+    event.stopPropagation();
+  };
+
+  console.log(openMenu);
+
   return (
     <div className="nav-container shadow-sm">
       <Container>
@@ -23,7 +39,7 @@ const Navbar = () => {
 
               <button className="primary-cta-btn">
                 <a
-                  href="https://play.google.com/store/apps" // Replace with your Play Store link
+                  href="https://play.google.com/store/apps"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -32,12 +48,52 @@ const Navbar = () => {
               </button>
             </ul>
 
-            <div className="mobile-menu">
+            <div className="mobile-menu" onClick={handleMenuPress}>
               <img src="/assets/mobile-menu.png" alt="menu" />
             </div>
           </div>
         </nav>
       </Container>
+
+      {openMenu && (
+        <div
+          className={`mobile-navigations ${openMenu ? "open" : ""}`}
+          onClick={handleMobileNavigationsClick}
+        >
+          <div
+            className={`menu-container ${openMenu ? "open" : ""}`}
+            onClick={stopPropagation}
+          >
+            <div className="close-btn" onClick={handleMenuPress}>
+              <img src="/assets/close.png" alt="close" />
+            </div>
+            <div className="logo-container">
+              <img src="/assets/doqo-logo-without-shadow.svg" alt="logo" />
+            </div>
+            <ul className="links-container">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+
+              <button className="primary-cta-btn">
+                <a
+                  href="https://play.google.com/store/apps"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Download App
+                </a>
+              </button>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
