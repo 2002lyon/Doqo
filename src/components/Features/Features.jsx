95d67dff6ec4ed54c5856119fc978e1c395be9a6
Leon <leon.lama2002@gmail.com>
2024-10-01 12:25:09 +0545

    experimental design client changes

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -3,24 +3,26 @@ import { features } from "../../constant/features";
 
 const Features = () => {
   return (
-    <Container>
-      <div className="feature-container">
-        <h2 className="feature-title">Our Features</h2>
-        <div className="features">
-          {features.map((feat, index) => (
-            <div key={index} className="feature-box">
-              <div className="feature-image-container">
-                <img src={feat.imageUrl} alt={feat.featureName} />
+    <div className="main-feature-container">
+      <Container>
+        <div className="feature-container">
+          <h2 className="feature-title">What is Doqo?</h2>
+          <div className="features">
+            {features.map((feat, index) => (
+              <div key={index} className="feature-box">
+                <div className="feature-image-container">
+                  <img src={feat.imageUrl} alt={feat.featureName} />
+                </div>
+                <div className="feature-information-container">
+                  <h3>{feat.featureName}</h3>
+                  <p>{feat.description}</p>
+                </div>
               </div>
-              <div className="feature-information-container">
-                <h3>{feat.featureName}</h3>
-                <p>{feat.description}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
